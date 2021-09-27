ec5f138dbb7682df4ba2a1ff29d6e3dbe0dea4c0
Malika Kassen-Lao <71674426+malikasen@users.noreply.github.com>
2021-09-27 10:05:31 -0700

    Initialize feature/add-user

---
+++ app/src/Users.js
@@ -0,0 +1,59 @@
+import * as React from "react";
+
+import * as apiClient from "./apiClient";
+
+const Users = () => {
+  const [users, setUsers] = React.useState([]);
+
+  const loadUsers = async () => setUsers(await apiClient.getUsers());
+  const addUser = async (user) => {
+    await apiClient.addUser(user);
+    loadUsers();
+  };
+
+  React.useEffect(() => {
+    loadUsers();
+  }, []);
+
+  return (
+    <section>
+      <UserList {...{ users }} />
+      <AddUser {...{ addUser }} />
+    </section>
+  );
+};
+
+const UserList = ({ users }) => (
+  <ul>
+    {users.map(({ id, name, email }) => (
+      <li key={id}>
+        {name} ({email})
+      </li>
+    ))}
+  </ul>
+);
+
+const AddUser = ({ addUser }) => {
+  const onSubmit = (e) => {
+    const form = e.currentTarget;
+    const user = Object.fromEntries(new FormData(form).entries());
+
+    e.preventDefault();
+    addUser(user);
+    form.reset();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>
+        Name: <input name="name" required />
+      </label>
+      <label>
+        Email: <input name="email" type="email" required />
+      </label>
+      <button>Add user</button>
+    </form>
+  );
+};
+
+export default Users;
